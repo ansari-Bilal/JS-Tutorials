4ba4fe9fc64c3c043a73726961429d8030638e71
ansari-Bilal <bilal5459ansari@gmail.com>
2024-01-05 19:03:14 +0530

    Numbers and Math Library in JS

---
+++ JS Basics/Num_&_Maths.js
@@ -0,0 +1,44 @@
+// const score=50
+// const balance=new Number(50)
+
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+// const num1=123.525461
+// console.log(num1.toPrecision(3)); // used for Round Off of numbers
+
+// const int=5165165161
+// console.log(int.toLocaleString());
+
+// +++++++++++++++++++++++ Math Library +++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));  //absolute value
+// console.log(Math.round(4.5)); // round off 
+// console.log(Math.ceil(5));  //directly convert the decimal to next number
+
+// console.log(Math.floor(5.9));
+
+// console.log(Math.sqrt(9));
+
+// console.log(Math.max(1,5,6,2,10,8)); 
+
+// console.log(Math.min(1,5,6,2,10,8)); 
+
+// console.log(Math.random()); //it gives any random value between 0 to 1
+// console.log(Math.random()*10);
+
+console.log( (Math.random()*10) + 1); // this will give random values from 1 to 9
+
+const min=10
+const max=20
+
+console.log( Math.floor(Math.random() * (max-min + 1) + min));  //gives values within a range of minimum and maxm
+
+
+
+
+
+
+
