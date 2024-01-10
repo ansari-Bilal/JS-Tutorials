404e0dc20920327ea1132e6ddf8a1b2adba95c55
ansari-Bilal <bilal5459ansari@gmail.com>
2024-01-10 20:08:53 +0530

    Object de-structure & JSON and API intro

---
+++ JS Basics 2/objects-3.js
@@ -0,0 +1,35 @@
+
+// Object de-structure and JSON API
+
+const course={
+    cousrename:"JS in Hindi",
+    price:"999",
+    cousreTeacher:"Hitesh Bhiya"
+}
+
+// const{cousreTeacher}=course         //here we are destructuring the object
+//console.log(courseTeacher);
+
+const{cousreTeacher : Teacher}=course
+console.log(Teacher);
+
+
+// <<<<< API  >>>>>
+
+// {                       //JSON in this way like objects
+//     "name":"bilal",     // & here both key and value are in STring format
+//     "college":"AEC",     
+//     "address":"Jamuria"
+    
+// }
+
+
+// [         //JSON can also be written in form of arrays of Objects
+//     {},
+//     {},
+//     {}
+// ]
+
+
+
+
