15800cf336b9661efad94dc281bb4e790704419b
ansari-Bilal <bilal5459ansari@gmail.com>
2024-01-10 21:53:20 +0530

    Scope level and Hoisting

--- JS Basics 3/scope.js
+++ JS Basics 3/scope.js
@@ -38,3 +38,20 @@ console.log(c);
 //"var" has a disadvantage as it changed the should come 300 as it var c=300 is in the global scope but the output is coming as 30 which is in the local scope      
 // so we avoid using "var" and prefer "let" and "const"
 
+
+
+
+// <------- Hoisting--------->
+
+
+console.log(addOne(5));       //hoisting and this will givethe correct answer
+function addOne(num){
+    return num+1
+}
+
+
+
+addtwo(5)       //this will show error in this case bcoz here the function is declare inside a variable 
+const addtwo = function(num2){   //we can declare function wrapped in a varibale also
+    return num2+2
+}
