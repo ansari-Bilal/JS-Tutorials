d95e0899bac56854d5f00b56d47d1bff1510ea4c
ansari-Bilal <bilal5459ansari@gmail.com>
2024-02-24 20:37:57 +0530

    some code added in Promise

--- Advance topic/Promise.js
+++ Advance topic/Promise.js
@@ -62,3 +62,42 @@ promise4                           //we can also write .then or .catch in anothe
 .finally(()=> console.log("Finally Block"))
 
 
+const promise5=new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error=true;
+        if(!error){
+        resolve({username:"xyz", password:"987"})
+        }else{
+            reject("xyz error found")
+        }
+    },1000);
+});
+
+async function consumePromise5(){
+    try {
+        const response = await promise5
+        console.log(response)
+    } catch (error) {
+        console.log(error);
+    }
+};
+consumePromise5();
+
+
+// async function getAllUSers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users');
+//         const data=await response.json();
+//         console.log(data);
+    
+//     } catch (error) {
+//         console.log("Errrr: ",error);
+//     }
+// }
+
+fetch('https://api.github.com/users/ansari-bilal')
+.then((response)=>{
+    return response.json()
+})
+.then((response)=> console.log(response))
+.catch((error)=> console.log("Errorrrrr:",error));
